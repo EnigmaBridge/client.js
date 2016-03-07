57323c3bbf0068ec20a3e738218829adab83c5e9
Dusan Klinec <dusan.klinec@gmail.com>
2016-03-07 17:02:48 +0100

    REST request building

--- enigma.js
+++ enigma.js
@@ -9,3 +9,27 @@ String.prototype.hexEncode = function(){
 
     return result
 };
+
+String.prototype.hexDecode = function(){
+    var j;
+    var hexes = this.match(/.{1,4}/g) || [];
+    var back = "";
+    for(j = 0; j<hexes.length; j++) {
+        back += String.fromCharCode(parseInt(hexes[j], 16));
+    }
+
+    return back;
+};
+
+function ebGenNonce(length){
+    var nonce = "";
+    var alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
+    var alphabetLen = alphabet.length;
+    var i = 0;
+
+    for(i = 0; i < length; i++){
+        nonce += alphabet.charAt(Math.floor(Math.random() * alphabetLen));
+    }
+
+    return nonce;
+}
