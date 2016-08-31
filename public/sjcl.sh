d16609a55fedc8f523e7562c35c0045a3b8f7a7b
Dusan Klinec <dusan.klinec@gmail.com>
2016-08-31 15:04:37 +0200

    CJS-32: sjcl build script updated

--- public/sjcl.sh
+++ public/sjcl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone git@github.com:bitwiseshiftleft/sjcl.git sjcl
 cd sjcl
-./configure --with-bn --with-ecc --with-codecBytes --with-ctr
+./configure --with-codecBase32 --with-bn --with-codecBase32 --with-codecBytes --with-ctr --with-sha1
 make
 cd sjcl.js ../
