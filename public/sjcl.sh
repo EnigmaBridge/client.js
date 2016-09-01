e6f496ea00d0801e497ac016895bd8baa7a2fe43
Dusan Klinec <dusan.klinec@gmail.com>
2016-09-01 21:41:49 +0200

    CJS-32: repository cleaning

--- public/sjcl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone git@github.com:bitwiseshiftleft/sjcl.git sjcl
-cd sjcl
-./configure --with-codecBase32 --with-bn --with-codecBase32 --with-codecBytes --with-ctr --with-sha1
-make
-cd sjcl.js ../
