48e8ae54bd47b69af43183073e0fc995fe554ab8
Dusan Klinec <dusan.klinec@gmail.com>
2016-09-02 07:45:32 +0200

    CJS-32: request endpoint configurable with URL
    - https://site2.enigmabridge.com:11180 is analyzed, scheme, hostname and port are extracted.
    - url package is used

--- build/build.js
+++ build/build.js
@@ -12,9 +12,9 @@ shell.exec("cd ./node_modules/sjcl/ && ./configure " +
 shell.exec("cd ./node_modules/sjcl/ && make");
 
 // Browserify.
-shell.exec("./node_modules/.bin/browserify --no-builtins --standalone eb lib/enigma.js > dist/enigma.js");
+shell.exec("./node_modules/.bin/browserify --no-builtins --standalone eb -r url lib/enigma.js > dist/enigma.js");
 
 // Browserify + minify
-shell.exec("./node_modules/.bin/browserify --no-builtins --standalone eb lib/enigma.js " +
+shell.exec("./node_modules/.bin/browserify --no-builtins --standalone eb -r url lib/enigma.js " +
     "-d -p [minifyify --map dist/enigma.js.map --output dist/enigma.js.map] > dist/enigma.min.js");
 
