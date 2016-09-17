b88970d96ba3394cf86d9a46fd16dac1e7020dd1
Dusan Klinec <dusan.klinec@gmail.com>
2016-09-17 12:42:23 +0200

    [build]: sjcl_compress added, travis build fixed

--- build/build.js
+++ build/build.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
+var fs = require('fs');
 
 // Build SJCL with required modules
 shell.exec("cd ./node_modules/sjcl/ && ./configure " +
@@ -8,6 +9,13 @@ shell.exec("cd ./node_modules/sjcl/ && ./configure " +
     "--with-codecBase32 " +
     "--with-codecBytes " +
     "--with-sha1");
+
+// Make sure SJCL's compress directory exists
+if (!fileExists('./node_modules/sjcl/compress/compress_with_closure.sh')){
+    shell.exec("mkdir -p ./node_modules/sjcl/compress " +
+        " && cp -a build/sjcl_compress/* ./node_modules/sjcl/compress/ " +
+        " && echo Compress copied");
+}
 
 // Make & Backup specially modified SJCL to the lib
 shell.exec("cd ./node_modules/sjcl/ && make " +
@@ -22,3 +30,11 @@ shell.exec("mkdir -p dist/ && ./node_modules/.bin/browserify --exclude crypto --
 shell.exec("./node_modules/.bin/browserify --exclude crypto --standalone eb lib/enigma.js " +
     "-d -p [minifyify --map dist/enigma.js.map --output dist/enigma.js.map] > dist/enigma.min.js");
 
+
+function fileExists(filePath) {
+    try {
+        return fs.statSync(filePath).isFile();
+    } catch (err) {
+        return false;
+    }
+}
